0568cc5367f44d2f0b42c0439a31fbbfa61660b1
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-07-18 15:14:58 -0300

    feat: add auth page

---
+++ src/pages/auth.tsx
@@ -0,0 +1,10 @@
+import AuthLayout from '@/components/Layouts/AuthLayout'
+import Login from '@/components/pages/Login'
+
+export default function Auth() {
+  return (
+    <AuthLayout>
+      <Login />
+    </AuthLayout>
+  )
+}
