1988d623f7e212ce5c4943973f44b93bf6c7d132
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-07-18 15:15:20 -0300

    refactor: change bg from body

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -76,7 +76,7 @@ const GlobalStyles: GlobalStyleComponent<
 
       ${!removeBg &&
       css`
-        background-color: ${theme.colors.mainBg};
+        background-color: ${theme.colors.baseColorGray2};
       `}
     }
   `}
