7bed1e28bb916ff37a9b93a5e942ef0b504cd0c2
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-07-21 12:00:44 -0300

    feat: add route account debit

---
+++ src/pages/accounts-debit.tsx
@@ -0,0 +1,10 @@
+import DefaultLayout from '@/components/Layouts/DefaultLayout'
+import AccountsDebitComponent from '@/components/pages/AccountsDebit'
+
+export default function AccountsDebit() {
+  return (
+    <DefaultLayout>
+      <AccountsDebitComponent />
+    </DefaultLayout>
+  )
+}
