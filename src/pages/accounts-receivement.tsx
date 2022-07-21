74b0e99cff0452143b529e87e75b1cd3363c7a03
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-07-21 12:00:55 -0300

    feat: add route account receivement

---
+++ src/pages/accounts-receivement.tsx
@@ -0,0 +1,10 @@
+import DefaultLayout from '@/components/Layouts/DefaultLayout'
+import AccountsReceivementComponent from '@/components/pages/AccountsReceivement'
+
+export default function AccountsReceivement() {
+  return (
+    <DefaultLayout>
+      <AccountsReceivementComponent />
+    </DefaultLayout>
+  )
+}
