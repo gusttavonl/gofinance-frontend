b05dc8a1fc710b80f88047ffa2e9dfb2620c216c
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-07-21 12:01:19 -0300

    feat: add route categories receivement

---
+++ src/pages/categories-receivement.tsx
@@ -0,0 +1,10 @@
+import DefaultLayout from '@/components/Layouts/DefaultLayout'
+import CategoriesReceivementComponent from '@/components/pages/CategoriesReceivement'
+
+export default function CategoriesReceivement() {
+  return (
+    <DefaultLayout>
+      <CategoriesReceivementComponent />
+    </DefaultLayout>
+  )
+}
