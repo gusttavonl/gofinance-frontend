bc508f4eed35d2aceb74ac8e0d5dc902906cfca8
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-07-21 12:00:21 -0300

    feat: add page categories receivement

---
+++ src/components/pages/CategoriesDebit/index.tsx
@@ -0,0 +1,10 @@
+import * as S from './styles'
+import ListCategories from '@/components/ListCategories'
+
+const CategoriesDebit = () => (
+  <S.Wrapper>
+    <ListCategories />
+  </S.Wrapper>
+)
+
+export default CategoriesDebit
