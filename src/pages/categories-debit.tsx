6746b3a2b1fdf12401e5c9f014f4033c0c57af8b
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-07-21 12:01:08 -0300

    feat: add route categories debit

---
+++ src/pages/categories-debit.tsx
@@ -0,0 +1,10 @@
+import DefaultLayout from '@/components/Layouts/DefaultLayout'
+import CategoriesDebitComponent from '@/components/pages/CategoriesDebit'
+
+export default function CategoriesDebit() {
+  return (
+    <DefaultLayout>
+      <CategoriesDebitComponent />
+    </DefaultLayout>
+  )
+}
