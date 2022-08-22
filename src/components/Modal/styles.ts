2e59a7efcdfd48b4e8e876a0033289cfd22b291c
GustavoNoronha0 <admin@gustavonoronha.com.br>
2022-08-22 18:05:36 -0300

    feat: add modal component styles

---
+++ src/components/Modal/styles.ts
@@ -0,0 +1,55 @@
+import styled, { css } from 'styled-components'
+import { RiMoneyDollarCircleLine } from 'react-icons/ri'
+
+export const Wrapper = styled.div.attrs({
+  'data-testid': 'WrapperCard'
+})`
+  height: 50vh;
+  width: 60vw;
+
+  ${({ theme }) => css`
+    background: ${theme.colors.white};
+  `}
+  box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.2);
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  margin-top: 10px;
+  
+  border-radius: 7px;
+
+  position: relative;
+`
+
+export const Header = styled.div`
+`
+
+export const Title = styled.h3`
+  ${({ theme }) => css`
+    font-family: ${theme.font.family};
+    color: ${theme.colors.black};
+  `}
+`
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 15px;
+`
+
+export const IconDollar = styled(RiMoneyDollarCircleLine)`
+  ${({ theme }) => css`
+    color: ${theme.colors.baseColorYellow};
+  `}
+`
+
+export const Count = styled.h2`
+  ${({ theme }) => css`
+    font-family: ${theme.font.family};
+    color: ${theme.colors.black};
+    margin: 5px 0 0 5px;
+  `}
+`
