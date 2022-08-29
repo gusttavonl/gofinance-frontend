eee6b6c863bb57ebc51702be76956511ccac3fc8
GustavoNoronha0 <admin@gustavonoronha.com.br>
2022-08-29 18:49:27 -0300

    feat: add categories receivement form component

---
+++ src/components/pages/CategoriesReceivement/components/CategoriesReceivementForm/index.tsx
@@ -0,0 +1,17 @@
+import Button from '@/components/Button'
+import Input from '@/components/Input'
+import * as S from './styles' 
+
+export default function CategoriesReceivementForm() {
+  return (
+    <S.Container>
+      <S.Inputs>
+        <Input name='title' label='Titulo' placeholder='Digite o titulo'/>
+        <Input name='description' label='Descricao' placeholder='Digite a descricao'/>
+      </S.Inputs>
+      <Button>
+         Salvar
+        </Button>
+    </S.Container>
+  )
+}
---
+++ src/components/pages/CategoriesReceivement/components/CategoriesReceivementForm/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+`
+export const Inputs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 320px));
+  grid-row-gap: 10px;
+  justify-content: center;
+`
