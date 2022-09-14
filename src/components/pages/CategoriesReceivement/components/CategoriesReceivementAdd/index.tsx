4792b1fb93a13499e550ab16fbf7df1c45861327
GustavoNoronha0 <admin@gustavonoronha.com.br>
2022-09-13 21:01:27 -0300

    refactor: change title of categories receivement add

--- src/components/pages/CategoriesReceivement/components/CategoriesReceivementAdd/index.tsx
+++ src/components/pages/CategoriesReceivement/components/CategoriesReceivementAdd/index.tsx
@@ -10,7 +10,7 @@ export type CategoriesReceivementAddProps = {
 const CategoriesReceivementAdd = ({ isOpen, getValueOpen }: CategoriesReceivementAddProps) => {
   return (
     <Modal 
-      title="Adicionar novo recebimento"
+      title="Adicionar nova categoria de recebimento"
       isOpen={isOpen}
       getValueOpen={getValueOpen}
     >
