244de8d50f6a76981f92964393ecd2e26905fb4c
GustavoNoronha0 <admin@gustavonoronha.com.br>
2022-09-13 21:00:20 -0300

    feat: add categories debit form index

---
+++ src/components/pages/CategoriesDebit/components/CategoriesDebitForm/index.tsx
@@ -0,0 +1,19 @@
+import Button from '@/components/Button'
+import Input from '@/components/Input'
+import * as S from './styles' 
+
+export default function CategoriesDebitForm() {
+  return (
+    <S.Container>
+      <S.Inputs>
+        <Input name='title' label='Titulo' placeholder='Digite o titulo'/>
+        <Input name='description' label='Descricao' placeholder='Digite a descricao'/>
+      </S.Inputs>
+      <S.ButtonArea>
+        <Button>
+          Salvar
+        </Button>
+      </S.ButtonArea>
+    </S.Container>
+  )
+}
