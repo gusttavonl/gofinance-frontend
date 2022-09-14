6216abce0d033144cf668a54d2b258122a1734e2
GustavoNoronha0 <admin@gustavonoronha.com.br>
2022-09-13 21:00:56 -0300

    feat: change categories debit to receive modal

--- src/components/pages/CategoriesDebit/index.tsx
+++ src/components/pages/CategoriesDebit/index.tsx
@@ -1,10 +1,23 @@
 import * as S from './styles'
 import ListCategories from '@/components/ListCategories'
+import { useState } from 'react'
+import CategoriesDebitAdd from './components/CategoriesDebitAdd'
 
-const CategoriesDebit = () => (
-  <S.Wrapper>
-    <ListCategories />
-  </S.Wrapper>
-)
+const CategoriesDebit = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const getValueOpen = (value: boolean) => {
+    setModalIsOpen(value)
+  }
+  return (
+    <>
+      <S.Wrapper>
+        <ListCategories openModal={getValueOpen} />
+      </S.Wrapper>
+      <S.ModalArea>
+        <CategoriesDebitAdd getValueOpen={getValueOpen} isOpen={modalIsOpen} />
+      </S.ModalArea>
+    </>
+  )
+}
 
 export default CategoriesDebit
