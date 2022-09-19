99b268bac082bd2ae7f74a33ed6628259346dd0b
GustavoNoronha0 <admin@gustavonoronha.com.br>
2022-09-19 18:06:57 -0300

    refactor: change accounts receivement to add modal add

--- src/components/pages/AccountsReceivement/index.tsx
+++ src/components/pages/AccountsReceivement/index.tsx
@@ -1,10 +1,23 @@
 import * as S from './styles'
 import ListAccounts from '@/components/ListAccounts'
+import { useState } from 'react'
+import AccountsReceivementAdd from './components/AccountsReceivementAdd'
 
-const AccountsReceivement = () => (
-  <S.Wrapper>
-    <ListAccounts />
-  </S.Wrapper>
-)
+const AccountsReceivement = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const getValueOpen = (value: boolean) => {
+    setModalIsOpen(value)
+  }
+  return (
+    <>
+      <S.Wrapper>
+        <ListAccounts openModal={getValueOpen} />
+      </S.Wrapper>
+      <S.ModalArea>
+        <AccountsReceivementAdd getValueOpen={getValueOpen} isOpen={modalIsOpen} />
+      </S.ModalArea>
+    </>
+  )
+}
 
 export default AccountsReceivement
