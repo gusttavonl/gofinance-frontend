85220badfe25431dd0be1b6f3e94b73ce8dcfd51
GustavoNoronha0 <admin@gustavonoronha.com.br>
2022-09-22 16:40:17 -0300

    feat: add modal on account debit

--- src/components/pages/AccountsDebit/index.tsx
+++ src/components/pages/AccountsDebit/index.tsx
@@ -1,10 +1,23 @@
 import * as S from './styles'
 import ListAccounts from '@/components/ListAccounts'
+import { useState } from 'react'
+import AccountsDebitAdd from './components/AccountsDebitAdd'
 
-const AccountsDebit = () => (
-  <S.Wrapper>
-    <ListAccounts />
-  </S.Wrapper>
-)
+const AccountsDebit = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const getValueOpen = (value: boolean) => {
+    setModalIsOpen(value)
+  }
+  return (
+    <>
+      <S.Wrapper>
+        <ListAccounts openModal={getValueOpen} />
+      </S.Wrapper>
+      <S.ModalArea>
+        <AccountsDebitAdd getValueOpen={getValueOpen} isOpen={modalIsOpen} />
+      </S.ModalArea>
+    </>
+  )
+}
 
 export default AccountsDebit
